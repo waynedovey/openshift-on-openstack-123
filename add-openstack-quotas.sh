fdd552fb495a2f609d619b3426224dd46313db69
Mike Hepburn <eformat@gmail.com>
2019-08-22 08:52:12 +1000

    🌴 instance quotas  🌴

--- add-openstack-quotas.sh
+++ add-openstack-quotas.sh
@@ -3,4 +3,4 @@ lvextend -l +4607 /dev/mapper/vg0-var
 resize2fs /dev/mapper/vg0-var
 openstack quota set --ram 102400 openshift
 openstack quota set --cores 40 openshift
-
+openstack quota set --instances 20 openshift
